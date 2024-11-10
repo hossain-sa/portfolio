164894171acda5fc24037ab4f36ae7d3c876d9ca
arifhossain-saeed <arifhossain.saeed@hotmail.com>
2024-11-10 16:29:07 +0000

    made several design changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("back-to-top").style.display="none";
   const navItems = document.querySelectorAll(".nav-item");
   let currentNavItem;
   navItems.forEach(item => {
@@ -9,3 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   currentNavItem.classList.add("active-link");
 });
+
+document.addEventListener("scroll", (e)=>{
+  e.preventDefault();
+  if(window.scrollY > 0){
+    document.getElementById("back-to-top").style.display="flex";
+  }else{
+    document.getElementById("back-to-top").style.display="none";
+  }
+});
+
+document.getElementById("back-to-top").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo(0, 0);
+})
+
+
